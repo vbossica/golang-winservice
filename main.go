ee1a475865d7e91d9199ac418a87d3e4666ad40d
Vladimir Ritz Bossicard <vbossica+github@gmail.com>
2025-05-20 18:26:01 +0200

    delete manage.go and install.go

--- main.go
+++ main.go
@@ -15,25 +15,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"os"
-	"strings"
 
 	"golang.org/x/sys/windows/svc"
 )
 
-func usage(errmsg string) {
-	fmt.Fprintf(os.Stderr,
-		"%s\n\n"+
-			"usage: %s <command>\n"+
-			"       where <command> is one of\n"+
-			"       install, remove, debug, start, stop, pause or continue.\n",
-		errmsg, os.Args[0])
-	os.Exit(2)
-}
-
-var svcName = "exampleservice"
+var svcName = "mysampleservice"
 
 func main() {
 	flag.StringVar(&svcName, "name", svcName, "name of the service")
@@ -48,32 +35,5 @@ func main() {
 		return
 	}
 
-	if len(os.Args) < 2 {
-		usage("no command specified")
-	}
-
-	cmd := strings.ToLower(os.Args[1])
-	switch cmd {
-	case "debug":
-		RunService(svcName, true)
-		return
-	case "install":
-		err = InstallService(svcName, "example service")
-	case "remove":
-		err = RemoveService(svcName)
-	case "start":
-		err = StartService(svcName)
-	case "stop":
-		err = ControlService(svcName, svc.Stop, svc.Stopped)
-	case "pause":
-		err = ControlService(svcName, svc.Pause, svc.Paused)
-	case "continue":
-		err = ControlService(svcName, svc.Continue, svc.Running)
-	default:
-		usage(fmt.Sprintf("invalid command %s", cmd))
-	}
-	if err != nil {
-		log.Fatalf("failed to %s %s: %v", cmd, svcName, err)
-	}
-	return
+	log.Fatal("Can only be run as a Windows service")
 }
